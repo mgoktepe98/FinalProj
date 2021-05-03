2763268c8d54a8e729f18f0bd2bf54b2f20c5634
aochoa5 <55251984+aochoa5@users.noreply.github.com>
2021-05-02 22:46:09 -0400

    Add files via upload

---
+++ cc.js
@@ -0,0 +1,24 @@
+var input = document.getElementById('cardno');
+var card = document.getElementById('card-type');
+
+//Regex constants
+const mc = /^(5[1-5])/gm;
+const visa = /^4/gm;
+const ae = /^(3[4,7])/gm;
+const disc = /^6/gm;
+const empty = /^$/gm;
+
+//On user key up check regex and display card type.
+input.onkeyup = function() {
+    if(mc.test(input.value)){
+    card.innerHTML = "Master Card";
+    }else if(visa.test(input.value)){
+      card.innerHTML = "Visa";
+    }else if(ae.test(input.value)){
+      card.innerHTML = "American Express";
+    }else if(disc.test(input.value)){
+      card.innerHTML = "Discover";
+    }else if(empty.test(input.value)){
+      card.innerHTML = "";
+    }
+}
